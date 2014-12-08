d27470f0643391e17dd65444b66d1a19f71a58a8
marc <marcmascarell@gmail.com>
2014-12-08 23:55:09 +0100

    title login

--- src/views/pages/login.blade.php
+++ src/views/pages/login.blade.php
@@ -2,7 +2,7 @@
 <html class="bg-black">
     <head>
         <meta charset="UTF-8">
-        <title>AdminLTE | Log in</title>
+        <title>Admin | Log in</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
         <!-- bootstrap 3.0.2 -->

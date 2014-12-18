f26a424d4768f63898210dd3f944add4645fe741
Marc Mascarell <marcmascarell@gmail.com>
2014-12-18 22:15:51 +0100

    move moment.js to the widget that needs it

--- src/views/partials/head-scripts.blade.php
+++ src/views/partials/head-scripts.blade.php
@@ -34,7 +34,7 @@
 
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 {{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/AdminLTE/dashboard.js') }}" type="text/javascript"></script>--}}
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/moment-with-locales.min.js') }}" type="text/javascript"></script>--}}
+
 
 <script src="{{ asset('packages/mascame/artificer/core/restfulizer.js') }}"></script>
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/artificer.js') }}"></script>

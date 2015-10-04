b157ff5df94894d8d4a150c7ff473a39f5247d98
Marc Mascarell <marcmascarell@gmail.com>
2015-10-04 02:00:02 +0200

    - remove some not used scripts

--- resources/views/partials/head-scripts.blade.php
+++ resources/views/partials/head-scripts.blade.php
@@ -2,21 +2,10 @@
 @if (!$standalone)
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 
-
 <!-- jQuery UI 1.10.3 -->
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/jquery-ui-1.10.3.min.js') }}" type="text/javascript"></script>
 <!-- Bootstrap -->
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<!-- Morris.js charts -->
-{{--<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>--}}
-<!--	<script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/morris/morris.min.js') }}" type="text/javascript"></script>-->
-<!-- Sparkline (charts)-->
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/sparkline/jquery.sparkline.min.js') }}" type="text/javascript"></script>--}}
-<!-- jvectormap -->
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}" type="text/javascript"></script>--}}
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}" type="text/javascript"></script>--}}
-<!-- jQuery Knob Chart -->
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/jqueryKnob/jquery.knob.js') }}" type="text/javascript"></script>--}}
 <!-- daterangepicker -->
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/daterangepicker/daterangepicker.js') }}" type="text/javascript"></script>
 <!-- datepicker -->
@@ -28,27 +17,12 @@
 <!-- iCheck -->
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
 
-
 <!-- AdminLTE App -->
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/AdminLTE/app.js') }}" type="text/javascript"></script>
-
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-{{--<script src="{{ asset('packages/mascame/artificer-default-theme/js/AdminLTE/dashboard.js') }}" type="text/javascript"></script>--}}
-
 
 <script src="{{ asset('packages/mascame/admin/core/restfulizer.js') }}"></script>
 <script src="{{ asset('packages/mascame/artificer-default-theme/js/artificer.js') }}"></script>
 
-<!-- AdminLTE for demo purposes -->
-<!-- Can change color skin and toggle fixed layout -->
-<!-- <script src="{{ asset('packages/mascame/artificer-default-theme/js/AdminLTE/demo.js') }}" type="text/javascript"></script>-->
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js') }}"></script>
-<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js') }}"></script>
-<![endif]-->
 @endif
 
 {{ \Mascame\Artificer\Artificer::assets() }}

541e1a36ad7e7fac7c49c78a69539877d28e44f6
Marc Mascarell <marcmascarell@gmail.com>
2016-11-01 22:53:45 +0100

    Applied fixes from StyleCI (#1)

--- src/Mascame/ArtificerDefaultTheme/ArtificerDefaultThemeServiceProvider.php
+++ src/Mascame/ArtificerDefaultTheme/ArtificerDefaultThemeServiceProvider.php
@@ -1,47 +1,48 @@
-<?php namespace Mascame\ArtificerDefaultTheme;
+<?php
+
+namespace Mascame\ArtificerDefaultTheme;
 
 use Illuminate\Support\ServiceProvider;
 
-class ArtificerDefaultThemeServiceProvider extends ServiceProvider {
+class ArtificerDefaultThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('mascame/artificer-default-theme');
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('mascame/artificer-default-theme');
+        require_once __DIR__.'/../../views/macros/table.php';
+        require_once __DIR__.'/../../views/macros/field.php';
+    }
 
-        require_once __DIR__ . '/../../views/macros/table.php';
-        require_once __DIR__ . '/../../views/macros/field.php';
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
--- src/views/macros/field.php
+++ src/views/macros/field.php
@@ -1,11 +1,11 @@
 <?php
 
 HTML::macro('field', function ($field, $icon, $errors = false) {
-	?>
+    ?>
 	<div class="form-group">
 		<?php if (App::environment() != 'production') {
-			print '(' . $field->type . ')';
-		} ?>
+        echo '('.$field->type.')';
+    } ?>
 
 		<?php Event::fire('artificer.view.edit.before.title', $field, $halt = false); ?>
 
@@ -13,29 +13,32 @@ HTML::macro('field', function ($field, $icon, $errors = false) {
 
 		<?php Event::fire('artificer.view.edit.after.title', $field, $halt = false); ?>
 
-		<?php if ($field->wiki) { ?>
+		<?php if ($field->wiki) {
+        ?>
 			<div class="well well-sm">
 				<i class="<?= $icon['info'] ?>"></i>
 				&nbsp;
 				<em><?= $field->wiki ?></em>
 			</div>
-		<?php } ?>
+		<?php 
+    } ?>
 
 		<?php if ($errors && $errors->has()) {
-			foreach ($errors->get($field->name) as $message) {
-				print $message;
-			}
-		} ?>
+        foreach ($errors->get($field->name) as $message) {
+            echo $message;
+        }
+    } ?>
 
 		<?php Event::fire('artificer.view.edit.before.output', $field, $halt = false); ?>
 
-		<?php Event::fire('artificer.view.edit.field.' . $field->type . '.before.output', $field->value) ?>
+		<?php Event::fire('artificer.view.edit.field.'.$field->type.'.before.output', $field->value) ?>
 
 		<?= $field->output() ?>
 
-		<?php Event::fire('artificer.view.edit.field.' . $field->type . '.after.output', $field->value) ?>
+		<?php Event::fire('artificer.view.edit.field.'.$field->type.'.after.output', $field->value) ?>
 
 		<?php Event::fire('artificer.view.edit.after.output', $field, $halt = false); ?>
 	</div>
 	<?php
+
 });

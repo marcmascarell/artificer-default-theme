28a970a705f2d9c2eb3e99543b2f9397b98fbecc
Marc Mascarell <marcmascarell@gmail.com>
2016-11-05 19:49:34 +0100

    Applied fixes from StyleCI (#2)

--- resources/views/macros/field.php
+++ resources/views/macros/field.php
@@ -1,12 +1,12 @@
 <?php
 
 HTML::macro('field', function ($field, $icon, $errors = false) {
-	?>
+    ?>
 	<div class="form-group <?= ($errors->has($field->getName())) ? 'has-error' : ''; ?>">
 		<?php if (App::environment() != 'production') {
-//			print '(' . $field->type . ') > ';
+        //			print '(' . $field->type . ') > ';
 //			print ($field->isFillable()) ? 'fillable' : 'not-fillable';
-		} ?>
+    } ?>
 
 		<?php Event::fire('artificer.view.edit.before.title', $field, $halt = false); ?>
 
@@ -14,29 +14,32 @@ HTML::macro('field', function ($field, $icon, $errors = false) {
 
 		<?php Event::fire('artificer.view.edit.after.title', $field, $halt = false); ?>
 
-		<?php if ($field->wiki) { ?>
+		<?php if ($field->wiki) {
+        ?>
 			<div class="well well-sm">
 				<i class="<?= $icon['info'] ?>"></i>
 				&nbsp;
 				<em><?= $field->wiki ?></em>
 			</div>
-		<?php } ?>
+		<?php 
+    } ?>
 
 		<?php if ($errors && $errors->has($field->getName())) {
-			foreach ($errors->get($field->getName()) as $message) {
-				print "<div class='error'>$message</div>";
-			}
-		} ?>
+        foreach ($errors->get($field->getName()) as $message) {
+            echo "<div class='error'>$message</div>";
+        }
+    } ?>
 
 		<?php Event::fire('artificer.view.edit.before.output', $field, $halt = false); ?>
 
-		<?php Event::fire('artificer.view.edit.field.' . $field->type . '.before.output', $field->value) ?>
+		<?php Event::fire('artificer.view.edit.field.'.$field->type.'.before.output', $field->value) ?>
 
 		<?= $field->protectGuarded()->withWidgets()->output() ?>
 
-		<?php Event::fire('artificer.view.edit.field.' . $field->type . '.after.output', $field->value) ?>
+		<?php Event::fire('artificer.view.edit.field.'.$field->type.'.after.output', $field->value) ?>
 
 		<?php Event::fire('artificer.view.edit.after.output', $field, $halt = false); ?>
 	</div>
 	<?php
+
 });
